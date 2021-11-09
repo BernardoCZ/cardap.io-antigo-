6f9e68207459a56d4218f4a1414db66ae0f2034a
Bernardo Zietolie <bernardo.260204@bento.local>
2021-11-09 09:43:18 -0300

    separando pastas

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['pagina' => 'home']);
+    return view('login', ['pagina' => 'home']);
 })->name('home');
 
 Route::get('produtos', [ProdutosController::class, 'index'])->name('produtos');
