7a366a372705698581074faa5463e4560986583b
Bernardo Zietolie <bernardo.260204@bento.local>
2021-11-11 11:52:58 -0300

    aaa

---
+++ resources/views/produtos/search.blade.php
@@ -0,0 +1,19 @@
+@extends('templates.base')
+@section('title', 'Buscar Estabelecimento')
+
+@section('content')
+<div class="row">
+    <h1>Resultados:</h1>
+    <hr>
+</div>
+
+<div class="row">
+    <div class="card" style="margin-top: 25px">
+        <div class="card-body">
+            <h5 class="card-title">McDonalds</h5>
+            <p class="card-text">Fast-food</p>
+            <p class="card-text">Rua Ednaldo Pereira, 360 - Bento Gonçalves, RS</p>
+        </div>
+    </div>
+</div>
+@endsection
