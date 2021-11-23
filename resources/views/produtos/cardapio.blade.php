895135e235af8b3978d5a1f0370d0452a658e7b9
Bernardo Zietolie <bernardo.260204@bento.local>
2021-11-23 11:46:43 -0300

    Atualizando cardapio

--- resources/views/produtos/cardapio.blade.php
+++ resources/views/produtos/cardapio.blade.php
@@ -4,11 +4,23 @@
 @section('content')
 <div class="row">
     <div class="col">
-        <div class="p-4 p-md-5 mb-4 text-white rounded banner-img-background" style="background-image: url('../img/mcdonalds_example.png'); background-color: red;">
-            <div class="col-md-6 px-0">
+        <div class="d-grid p-4 mb-4 text-white rounded" style="grid-template-columns: 2fr 1fr; background-color: red;">
+            <div class="col-md px-0 p-md-5">
                 <h1 class="display-4 fst-italic">McDonald's</h1>
                 <p class="lead my-3">McDonald's Corporation é a maior cadeia mundial de restaurantes de fast food de hambúrguer.</p>
             </div>
+            <div class="banner-img-background" style="background-image: url('../img/mcdonalds_example.png');"></div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <div class="d-grid p-4 mb-4 text-white rounded" style="grid-template-columns: 2fr 1fr; background-color: red;">
+            <div class="col-md px-0 p-md-5">
+                <h1 class="display-4 fst-italic">McDonald's</h1>
+                <p class="lead my-3">McDonald's Corporation é a maior cadeia mundial de restaurantes de fast food de hambúrguer.</p>
+            </div>
+            <div class="banner-img-background" style="background-image: url('../img/mcdonalds_example.png');"></div>
         </div>
     </div>
 </div>
